f85457515aca1fd53cd78facdbfce269f0b4b892
ThiagoTrabach <thiago.trabach@gmail.com>
2025-04-29 06:03:12 -0300

    feat: Add SQL models for CAEPF, CNO, CNPJ, and CPF with deduplication logic and Airbyte integration

---
+++ models/raw/bcadastro/base/raw_base_bcadastro_chcaepf.sql
@@ -0,0 +1,49 @@
+-- tabela de CAEPF - Cadastro de Atividade Econômica da Pessoa Física
+
+{{
+    config(
+        alias="chcaepf_bcadastros_parsed",
+        schema="brutos_bcadastro_staging",
+        materialized="table",
+    )
+}}
+
+
+with
+    fonte as (
+        select *
+        from {{ source("brutos_bcadastro_staging", "chcaepf_bcadastros") }}
+
+        {% if target.name == "dev" %}
+            where
+                timestamp(_airbyte_extracted_at)
+                >= timestamp_sub(current_timestamp(), interval 3 day)
+        {% endif %}
+    ),
+
+    fonte_parseada as (
+        select
+
+
+            -- Airbyte fields
+            struct(
+                _airbyte_raw_id as raw_id,
+                _airbyte_extracted_at as extracted_at,
+                _airbyte_generation_id as generation_id,
+                nullif(json_value(_airbyte_meta, '$.changes'), "") as changes,
+                nullif(json_value(_airbyte_meta, '$.sync_id'), "") as sync_id
+            ) as airbyte,
+            
+        from fonte
+    ),
+
+    dedup as (
+        select *
+        from fonte_parseada
+        qualify
+            row_number() over (partition by cpfId order by airbyte.extracted_at desc)
+            = 1
+    )
+
+select *
+from dedup
