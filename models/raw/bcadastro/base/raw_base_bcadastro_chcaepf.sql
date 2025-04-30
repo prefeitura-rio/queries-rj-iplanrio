0b1ecce74d9d46aeb371b9481641694053c3b08c
ThiagoTrabach <thiago.trabach@gmail.com>
2025-04-30 04:38:27 -0300

    feat: Enhance SQL models for CAEPF and CNO by adding new fields extracted from JSON and updating deduplication logic

--- models/raw/bcadastro/base/raw_base_bcadastro_chcaepf.sql
+++ models/raw/bcadastro/base/raw_base_bcadastro_chcaepf.sql
@@ -1,5 +1,4 @@
 -- tabela de CAEPF - Cadastro de Atividade Econômica da Pessoa Física
-
 {{
     config(
         alias="chcaepf_bcadastros_parsed",
@@ -23,7 +22,49 @@ with
 
     fonte_parseada as (
         select
-
+            -- Campos extraídos do JSON conforme a imagem fornecida
+            nullif(json_value(doc, '$.bairro'), '') as bairro,
+            nullif(json_value(doc, '$.cei'), '') as cei,
+            nullif(json_value(doc, '$.cep'), '') as cep,
+            nullif(json_value(doc, '$.cnaes'), '') as cnaes,
+            nullif(json_value(doc, '$.codAtividade'), '') as codAtividade,
+            nullif(json_value(doc, '$.codMotivoSituacao'), '') as codMotivoSituacao,
+            nullif(json_value(doc, '$.codMunicipio'), '') as codMunicipio,
+            nullif(json_value(doc, '$.codOrgaoCpf'), '') as codOrgaoCpf,
+            nullif(json_value(doc, '$.codOrgaoMunicipio'), '') as codOrgaoMunicipio,
+            nullif(json_value(doc, '$.codQualificacao'), '') as codQualificacao,
+            nullif(json_value(doc, '$.codSituacao'), '') as codSituacao,
+            nullif(json_value(doc, '$.codSituacaoCpf'), '') as codSituacaoCpf,
+            nullif(json_value(doc, '$.codTipoContribuinte'), '') as codTipoContribuinte,
+            nullif(json_value(doc, '$.complemento'), '') as complemento,
+            safe.parse_date(
+                '%Y-%m-%d', nullif(json_value(doc, '$.dataInicioAtividade'), '')
+            ) as dataInicioAtividade,
+            safe.parse_date(
+                '%Y-%m-%d', nullif(json_value(doc, '$.dataSituacao'), '')
+            ) as dataSituacao,
+            nullif(json_value(doc, '$.emailContato'), '') as emailContato,
+            nullif(json_value(doc, '$.language'), '') as language,
+            nullif(json_value(doc, '$.logradouro'), '') as logradouro,
+            nullif(json_value(doc, '$.nomeAtividade'), '') as nomeAtividade,
+            nullif(json_value(doc, '$.nomeCpf'), '') as nomeCpf,
+            nullif(json_value(doc, '$.nomeMotivoSituacao'), '') as nomeMotivoSituacao,
+            nullif(json_value(doc, '$.nomeMunicipio'), '') as nomeMunicipio,
+            nullif(json_value(doc, '$.nomeQualificacao'), '') as nomeQualificacao,
+            nullif(json_value(doc, '$.nomeSituacao'), '') as nomeSituacao,
+            nullif(json_value(doc, '$.nomeSituacaoCpf'), '') as nomeSituacaoCpf,
+            nullif(json_value(doc, '$.nomeTipoContribuinte'), '') as nomeTipoContribuinte,
+            nullif(json_value(doc, '$.nroAepf'), '') as nroAepf,
+            nullif(json_value(doc, '$.nroAepfCompleto'), '') as nroAepfCompleto,
+            nullif(json_value(doc, '$.nroCpf'), '') as nroCpf,
+            nullif(json_value(doc, '$.numeroLogradouro'), '') as numeroLogradouro,
+            nullif(json_value(doc, '$.telefoneContato'), '') as telefoneContato,
+            nullif(json_value(doc, '$.timestamp'), '') as timestamp,
+            nullif(json_value(doc, '$.tipo'), '') as tipo,
+            nullif(json_value(doc, '$.ufMunicipio'), '') as ufMunicipio,
+            nullif(json_value(doc, '$.version'), '') as version,
+            nullif(json_value(doc, '$._id'), '') as _id,
+            nullif(json_value(doc, '$._rev'), '') as _rev,
 
             -- Airbyte fields
             struct(
@@ -33,7 +74,6 @@ with
                 nullif(json_value(_airbyte_meta, '$.changes'), "") as changes,
                 nullif(json_value(_airbyte_meta, '$.sync_id'), "") as sync_id
             ) as airbyte,
-            
         from fonte
     ),
 
@@ -41,7 +81,7 @@ with
         select *
         from fonte_parseada
         qualify
-            row_number() over (partition by cpfId order by airbyte.extracted_at desc)
+            row_number() over (partition by nroCpf order by airbyte.extracted_at desc)
             = 1
     )
 
