1e241ca08cb3a35c1b2b179fbf7545048fcd44f1
ThiagoTrabach <thiago.trabach@gmail.com>
2025-04-30 04:38:27 -0300

    feat: Enhance SQL models for CAEPF and CNO by adding new fields extracted from JSON and updating deduplication logic

--- models/raw/bcadastro/base/raw_base_bcadastro_chcno.sql
+++ models/raw/bcadastro/base/raw_base_bcadastro_chcno.sql
@@ -1,5 +1,4 @@
 -- tabela de CNO - Cadastro Nacional de Obras
-
 {{
     config(
         alias="chcno_bcadastros_parsed",
@@ -23,8 +22,46 @@ with
 
     fonte_parseada as (
         select
+            -- Campos extraídos do JSON conforme a imagem fornecida
+            nullif(json_value(doc, '$.areas'), '') as areas,
+            nullif(json_value(doc, '$.art'), '') as art,
+            nullif(json_value(doc, '$.bairro'), '') as bairro,
+            nullif(json_value(doc, '$.cadastromobiliario'), '') as cadastromobiliario,
+            nullif(json_value(doc, '$.cep'), '') as cep,
+            nullif(json_value(doc, '$.cib'), '') as cib,
+            nullif(json_value(doc, '$.cno'), '') as cno,
+            nullif(json_value(doc, '$.cnoVinculado'), '') as cnovinculado,
+            nullif(json_value(doc, '$.codigoMunicipio'), '') as codigomunicipio,
+            nullif(json_value(doc, '$.complemento'), '') as complemento,
+            safe.parse_date(
+                '%Y-%m-%d', nullif(json_value(doc, '$.dataInicioObra'), '')
+            ) as datainicioobra,
+            safe.parse_date(
+                '%Y-%m-%d', nullif(json_value(doc, '$.dataInicioResponsabilidade'), '')
+            ) as datainicioresponsabilidade,
+            safe.parse_date(
+                '%Y-%m-%d', nullif(json_value(doc, '$.dataSituacao'), '')
+            ) as datasituacao,
+            nullif(json_value(doc, '$.id_doc'), '') as id_doc,
+            nullif(json_value(doc, '$.logradouro'), '') as logradouro,
+            nullif(json_value(doc, '$.municipio'), '') as municipio,
+            nullif(json_value(doc, '$.niResponsavel'), '') as niresponsavel,
+            nullif(json_value(doc, '$.numeroLogradouro'), '') as numerologradouro,
+            nullif(json_value(doc, '$._rev'), '') as _rev,
+            nullif(json_value(doc, '$.rrt'), '') as rrt,
+            nullif(json_value(doc, '$.situacao'), '') as situacao,
+            nullif(json_value(doc, '$.timestamp'), '') as timestamp,
+            nullif(json_value(doc, '$.tipo'), '') as tipo,
+            nullif(json_value(doc, '$.tipoLogradouro'), '') as tipologradouro,
+            nullif(
+                json_value(doc, '$.tipoResponsabilidade'), ''
+            ) as tiporesponsabilidade,
+            nullif(json_value(doc, '$.uf'), '') as uf,
+            nullif(json_value(doc, '$.unidadeMedida'), '') as unidademedida,
+            nullif(json_value(doc, '$.valorMedida'), '') as valormedida,
+            nullif(json_value(doc, '$.version'), '') as version,
+            nullif(json_value(doc, '$._id'), '') as _id,
 
-        
             -- Airbyte fields
             struct(
                 _airbyte_raw_id as raw_id,
@@ -33,7 +70,6 @@ with
                 nullif(json_value(_airbyte_meta, '$.changes'), "") as changes,
                 nullif(json_value(_airbyte_meta, '$.sync_id'), "") as sync_id
             ) as airbyte,
-            
         from fonte
     ),
 
@@ -41,8 +77,7 @@ with
         select *
         from fonte_parseada
         qualify
-            row_number() over (partition by cpfId order by airbyte.extracted_at desc)
-            = 1
+            row_number() over (partition by cno order by airbyte.extracted_at desc) = 1
     )
 
 select *
