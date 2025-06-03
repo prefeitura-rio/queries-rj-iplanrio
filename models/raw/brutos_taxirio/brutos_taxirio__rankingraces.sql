d35cf428fcd09c2a1bd2aac0a909062b6de8d55e
Arnaldo Lima <arnaldo.lima@prefeitura.rio>
2025-06-03 11:33:36 -0300

    Correções em arquivos SQL

---
+++ models/raw/brutos_taxirio/brutos_taxirio__rankingraces.sql
@@ -0,0 +1,11 @@
+SELECT
+  SAFE_CAST (ano_particao as INT64) as ano_particao,
+  SAFE_CAST (mes_particao as INT64) as mes_particao,
+  SAFE_CAST (dia_particao as INT64) as dia_particao,
+  DATETIME (TIMESTAMP(createdAt)) as data_criacao,
+  DATETIME (TIMESTAMP(updatedAt)) as data_atualizacao,
+  SAFE_CAST (id as STRING) as id_ranking_corrida,
+  SAFE_CAST (race as STRING) as id_corrida,
+  PARSE_JSON (competitors) AS competidores
+FROM
+  `rj-iplanrio.brutos_taxirio_staging.rankingraces`
---
+++ models/raw/brutos_taxirio/brutos_taxirio__users.sql
@@ -0,0 +1,10 @@
+SELECT
+  SAFE_CAST (ano_particao as INT64) as ano_particao,
+  SAFE_CAST (mes_particao as INT64) as mes_particao,
+  SAFE_CAST (dia_particao as INT64) as dia_particao,
+  DATETIME (TIMESTAMP(createdAt)) as data_criacao,
+  SAFE_CAST (id as STRING) as id_usuario,
+  SAFE_CAST (federalRevenueData_sex as STRING) as sexo,
+  DATE(TIMESTAMP(birthDate)) as data_nascimento,
+FROM
+  `rj-iplanrio.brutos_taxirio_staging.users`
