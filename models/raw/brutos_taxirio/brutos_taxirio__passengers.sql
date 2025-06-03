6f77af189665390ef318dc272ae3849793ce6b7b
GeraldoSilvaMaiaJunior <geraldo.maia@prefeitura.rio>
2025-06-03 09:25:57 -0300

    Alteração nos modelos do Taxirio no ambiente de dev para atualizar  Tabela Passageiros

--- models/raw/brutos_taxirio/brutos_taxirio__passengers.sql
+++ models/raw/brutos_taxirio/brutos_taxirio__passengers.sql
@@ -9,19 +9,19 @@ SELECT
   SAFE_CAST (password as STRING) as senha,
   SAFE_CAST (salt as STRING) as aleatorio,
   SAFE_CAST (isAbleToUsePaymentInApp as BOOL) as pode_usar_pagamento_app,
-  DATETIME (TIMESTAMP(infoPhone_updatedAt)) as phone_atualizacao,
-  SAFE_CAST (infoPhone_id as STRING) as id_phone,
-  SAFE_CAST (infoPhone_appVersion as STRING) as phone_versao_app,
-  SAFE_CAST (infoPhone_phoneModel as STRING) as phone_modelo,
-  SAFE_CAST (infoPhone_phoneManufacturer as STRING) as phone_fabricante,
-  SAFE_CAST (infoPhone_osVersion as STRING) as phone_versao_sistema,
-  SAFE_CAST (infoPhone_osName as STRING) as phone_nome_sistema,
+  DATETIME (TIMESTAMP(infoPhone_updatedAt)) as data_atualizacao_telefone,
+  SAFE_CAST (infoPhone_id as STRING) as id_telefone,
+  SAFE_CAST (infoPhone_appVersion as STRING) as versao_app_telefone,
+  SAFE_CAST (infoPhone_phoneModel as STRING) as modelo_telefone,
+  SAFE_CAST (infoPhone_phoneManufacturer as STRING) as fabricante_telefone,
+  SAFE_CAST (infoPhone_osVersion as STRING) as versao_sistema_telefone,
+  SAFE_CAST (infoPhone_osName as STRING) as nome_sistema_telefone,
   SAFE_CAST (tokenInfo_httpSalt as STRING) as ficha_http_aleatorio,
   SAFE_CAST (tokenInfo_wssSalt as STRING) as ficha_wss_aleatorio,
   SAFE_CAST (tokenInfo_pushToken as STRING) as ficha_envio,
   SAFE_CAST (ano_particao as INT64) as ano_particao,
   SAFE_CAST (mes_particao as INT64) as mes_particao,
-  SAFE_CAST (dia_particao as INT64) as dia_particao,
+
   DATE(SAFE_CAST(ano_particao AS INT64), SAFE_CAST(mes_particao AS INT64), 1) AS data_particao
 FROM
   {{ source('brutos_taxirio_staging','passengers') }}
