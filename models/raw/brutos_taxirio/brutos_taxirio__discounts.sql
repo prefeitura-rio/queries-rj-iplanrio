62abf155ffd986c5694629dfe46fb64824b582f7
GeraldoSilvaMaiaJunior <geraldo.maia@prefeitura.rio>
2025-06-04 13:30:48 -0300

    Alteração nos modelos do Taxirio no ambiente de dev para apagar os schema antigo brutos_taxirio

--- models/raw/brutos_taxirio/brutos_taxirio__discounts.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT
-  SAFE_CAST (id as STRING) as id_desconto_associado,
-  SAFE_CAST (description as STRING) as descricao,
-  SAFE_CAST (value as FLOAT64) as desconto,
-  SAFE_CAST (createdAt as DATE) as data_criacao
-FROM {{ source('brutos_taxirio_staging', 'discounts') }}
--- models/raw/brutos_taxirio/brutos_taxirio__paymentmethods.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT
-  SAFE_CAST (id as STRING) as id_pagamento_associado,
-  SAFE_CAST (pindex as INT64) as pindex,
-  SAFE_CAST (name as STRING) as nome,
-  SAFE_CAST (type as STRING) as tipo
-FROM
-  {{ source('brutos_taxirio_staging','paymentmethods') }}
