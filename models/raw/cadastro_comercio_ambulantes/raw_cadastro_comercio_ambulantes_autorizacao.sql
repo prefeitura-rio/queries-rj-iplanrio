57c7e66f6fffbba9e007f311fe5e687755a3abff
Geraldo Maia <geraldo.maia@prefeitura.com>
2025-07-01 12:55:14 -0300

    Alteração no projeto DBT Cadastro de Ambulantes para completar os nomes dos campos

--- models/raw/cadastro_comercio_ambulantes/raw_cadastro_comercio_ambulantes_autorizacao.sql
+++ models/raw/cadastro_comercio_ambulantes/raw_cadastro_comercio_ambulantes_autorizacao.sql
@@ -1,6 +1,6 @@
 {{
     config(
-      schema="cadastro_comercio_ambulantes",
+      schema="brutos_cadastro_comercio_ambulantes",
       alias="autorizacao",
       materialized="table",
       tags=["raw", "cadastro_comercio_ambulantes"],
@@ -9,23 +9,23 @@
 }}
 
 SELECT 
-    safe_cast(id as integer) as id_autoriz_titular,
-    safe_cast(inscricaoMunicipal as string) as num_inscricao_municipal,
+    safe_cast(id as integer) as id_autorizacao_titular,
+    safe_cast(inscricaoMunicipal as string) as numero_inscricao_municipal,
     safe_cast(tipoPessoa as string) as tipo_pessoa,
     safe_cast(cpf as string) as cpf_titular,
-    safe_cast(nome as string) as nome_titular_autoriz,
-    safe_cast(situacao as string) as sit_inscricao_municipal,
-    safe_cast(dt_concessao as datetime) as dt_concessao,
-    safe_cast(dt_cancelamento as datetime) as dt_cancelamento,
-    safe_cast(codigoLogradouroPonto as string) as end_corporativo_cod_logradouro_ponto,
-    safe_cast(logradouroPonto as string) as end_corporativo_logradouro_ponto,
-    safe_cast(referenciaPonto as string) as end_corporativo_referencia_ponto,
-    safe_cast(numeroPortaPonto as string) as end_corporativo_num_porta_ponto,
-    safe_cast(complementoPonto as string) as end_corpotativo_compl_ponto,
-    safe_cast(codigoBairroPonto as string) as end_cod_bairro_ponto,
-    safe_cast(bairroPonto as string) as end_bairro_ponto,
-    safe_cast(equipamento as string) as nom_equip_autorizado,
-    safe_cast(permanente as string) as ind_autoriz_permanente,
-    safe_cast(processaConcessaoIM as string) as num_processo_concessao,
-    safe_cast(processoSeletivo as string) as num_interno_agrupam_concessao
+    safe_cast(nome as string) as nome_titular_autorizacao,
+    safe_cast(situacao as string) as situacao_inscricao_municipal,
+    safe_cast(dt_concessao as datetime) as data_concessao,
+    safe_cast(dt_cancelamento as datetime) as data_cancelamento,
+    safe_cast(codigoLogradouroPonto as string) as endereco_corporativo_cod_logradouro_ponto,
+    safe_cast(logradouroPonto as string) as endereco_corporativo_logradouro_ponto,
+    safe_cast(referenciaPonto as string) as endereco_corporativo_referencia_ponto,
+    safe_cast(numeroPortaPonto as string) as endereco_corporativo_num_porta_ponto,
+    safe_cast(complementoPonto as string) as endereco_corporativo_complemento_ponto,
+    safe_cast(codigoBairroPonto as string) as endereco_corporativo_cod_bairro_ponto,
+    safe_cast(bairroPonto as string) as endereco_corporativo_bairro_ponto,
+    safe_cast(equipamento as string) as nome_equipamento_autorizado,
+    safe_cast(permanente as string) as indicador_autorizacao_permanente,
+    safe_cast(processaConcessaoIM as string) as numero_processo_concessao,
+    safe_cast(processoSeletivo as string) as numero_interno_agrupamento_concessao
 FROM {{ source('brutos_cadastro_comercio_ambulantes_staging', 'vw_autorizacao_datalake') }}
--- models/raw/cadastro_comercio_ambulantes/raw_cadastro_comercio_ambulantes_autorizacao_auxiliar.sql
+++ models/raw/cadastro_comercio_ambulantes/raw_cadastro_comercio_ambulantes_autorizacao_auxiliar.sql
@@ -1,6 +1,6 @@
 {{
     config(
-      schema="cadastro_comercio_ambulantes",
+      schema="brutos_cadastro_comercio_ambulantes",
       alias="autorizacao_auxiliares",
       materialized="table",
       tags=["raw", "cadastro_comercio_ambulantes"],
@@ -9,10 +9,11 @@
 }}
 
 select 
-    safe_cast(idAutorizacao as integer) as id_autoriz_titular,
-    safe_cast(inscricaoMunicipal as string) as num_inscricao_municipal,
-    safe_cast(nome as string) as nome_titular_autoriz,
+    safe_cast(idAutorizacao as integer) as id_autorizacao_titular,
+    safe_cast(inscricaoMunicipal as string) as numero_inscricao_municipal,
+    safe_cast(nome as string) as nome_titular_autorizacao,
     safe_cast(cpf as string) as cpf_auxiliar,
     safe_cast(tipo_pessoa as string) as tipo_pessoa,
-    safe_cast(inicioVigenciaAuxiliar as datetime) as data_inicio_vigencia_auxiliar
+    safe_cast(inicioVigenciaAuxiliar as datetime) as data_inicio_vigencia_auxiliar,
+    safe_cast(fimVigenciaAuxiliar as datetime) as data_fim_vigencia_auxiliar
 from {{ source('brutos_cadastro_comercio_ambulantes_staging', 'vw_autorizacao_auxiliares_datalake') }}  
