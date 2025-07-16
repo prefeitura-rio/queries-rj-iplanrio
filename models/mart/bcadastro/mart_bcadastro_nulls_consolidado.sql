8252d43827dd6f8724f17221fe8f376cad667543
Pedro Siqueira <pedro.siqueira@poli.ufrj.br>
2025-07-16 12:22:47 -0300

    chore: Test CI

--- models/mart/bcadastro/mart_bcadastro_nulls_consolidado.sql
+++ models/mart/bcadastro/mart_bcadastro_nulls_consolidado.sql
@@ -1,28 +1,21 @@
 {{
     config(
         alias="nulls_consolidado",
-        schema='debug_bcadastro',
+        schema="debug_bcadastro",
         materialized="table",
     )
 }}
 
 select 'cpf' as table, *
-from {{ ref('int_bcadastro_nulls_cpf') }}
+from {{ ref("int_bcadastro_nulls_cpf") }}
 union all
 select 'cnpj' as table, *
-from {{ ref('int_bcadastro_nulls_cnpj') }}
+from {{ ref("int_bcadastro_nulls_cnpj") }}
 union all
 select 'cno' as table, *
-from {{ ref('int_bcadastro_nulls_cno') }}
+from {{ ref("int_bcadastro_nulls_cno") }}
 union all
-
-
-
 
 
 select 'caepf' as table, *
-from {{ ref('int_bcadastro_nulls_caepf') }}
-
-
-
-
+from {{ ref("int_bcadastro_nulls_caepf") }}
