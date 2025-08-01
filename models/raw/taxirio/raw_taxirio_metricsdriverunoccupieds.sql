69b0e2d791520e13b9501a9f9a613221e82ee3b0
ThiagoTrabach <thiago.trabach@gmail.com>
2025-07-31 22:38:05 -0300

    refact: remove unused fields and update freshness settings in raw_taxirio models

--- models/raw/taxirio/raw_taxirio_metricsdriverunoccupieds.sql
+++ models/raw/taxirio/raw_taxirio_metricsdriverunoccupieds.sql
@@ -1,11 +1,6 @@
 {{
     config(
         alias="metricas_motorista_desocupado",
-        partition_by={
-            "field": "data_criacao_particao",
-            "data_type": "timestamp",
-            "granularity": "day",
-        },
         description="Medidas de disponibilidade de motoristas",
     )
 }}
@@ -13,8 +8,6 @@
 select
     safe_cast(id as string) as id_metricas_motoriasta_desocupado,
     safe_cast(driver as string) as id_motorista,
-    safe_cast(associateddiscount as string) as id_desconto_associado,
-    datetime(timestamp(datetime)) as data_criacao,
-    datetime(timestamp(datetime)) as data_criacao_particao
+    safe_cast(associateddiscount as string) as id_desconto_associado
 
 from {{ source("brutos_taxirio_staging", "metricsdriverunoccupieds") }}
