96b9428b35559ff5fef9fb741e5045ac852cd59c
Joao Pedro Oliveira <joaopsantos@pm.me>
2025-08-19 19:05:38 -0300

    adjust sources

--- models/raw/smas/cadunico/raw_cadunico_agendamentos.sql
+++ models/raw/smas/cadunico/raw_cadunico_agendamentos.sql
@@ -22,7 +22,7 @@ with source_data as (
         unidade_endereco,
         unidade_bairro,
         current_timestamp() as processed_at
-    from {{ source('brutos_data_metrica_staging', 'agendamentos_cadunico') }}
+    from {{ source('brutos_data_metrica', 'cadunico_agendamentos') }}
     where data_hora is not null
 )
 
