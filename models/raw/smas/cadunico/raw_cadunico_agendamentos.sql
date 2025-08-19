a6500ac5f569802c90adab1be57d91d210635d5c
Joao Pedro Oliveira <joaopsantos@pm.me>
2025-08-19 14:53:56 -0300

    add sql

---
+++ models/raw/smas/cadunico/raw_cadunico_agendamentos.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        schema="brutos_data_metrica",
+        materialized="table",
+        partition_by={
+            "field": "data_hora",
+            "data_type": "date"
+        }
+    )
+}}
+
+with source_data as (
+    select
+        id,
+        id_capacidade,
+        nome_completo,
+        cpf,
+        telefone,
+        tipo,
+        data_hora,
+        unidade_nome,
+        unidade_endereco,
+        unidade_bairro,
+        current_timestamp() as processed_at
+    from {{ source('brutos_data_metrica', 'agendamentos_cadunico') }}
+    where data_hora is not null
+)
+
+select * from source_data
