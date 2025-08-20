11748b8d54b6663563a868c389eab7ad6ce74583
PedroSiqueira1 <pedro.siqueira@poli.ufrj.br>
2025-08-20 19:19:36 -0300

    Add smtur

---
+++ models/raw/smtur/oferta_turistica/raw_oferta_turistica__santos_dumont.sql
@@ -0,0 +1,21 @@
+{{
+  config(
+    alias='santos_dumont',
+    materialized='view'
+  )
+}}
+
+with dados_com_linha as (
+  select 
+    string_field_0 as mes_ano,
+    string_field_1 as total_de_passageiros,
+    row_number() over() as linha
+  from {{ source('oferta_turistica', 'santos_dumont') }}
+)
+
+select 
+  mes_ano,
+  total_de_passageiros
+from dados_com_linha
+where linha > 1
+
