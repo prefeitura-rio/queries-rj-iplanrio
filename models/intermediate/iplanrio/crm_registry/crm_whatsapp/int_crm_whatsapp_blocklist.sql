3e85e61c14f5812e69cf102bf9c7befb02937110
patriciacatandi <patriciabcatandi@gmail.com>
2025-08-25 16:16:45 -0300

    fix: blocklist

--- models/intermediate/iplanrio/crm_registry/crm_whatsapp/int_crm_whatsapp_blocklist.sql
+++ models/intermediate/iplanrio/crm_registry/crm_whatsapp/int_crm_whatsapp_blocklist.sql
@@ -10,11 +10,13 @@
 ) }}
 
 SELECT
-  CAST(phone AS STRING) as contato_telefone,
-  profile_name as contato_nome,
-  date_time as data_bloqueio,
-  reason as razao_bloqueio,
-  DATE(date_time) as data_particao
+*
+--   CAST(phone AS STRING) as contato_telefone,
+--   profile_name as contato_nome,
+--   date_time as data_bloqueio,
+--   reason as razao_bloqueio,
+--   DATE(date_time) as data_particao
 -- TODO: mudar a origem do dado
 FROM `rj-crm-registry-dev.patricia___brutos_wetalkie.blocklist`
-WHERE phone IS NOT NULL AND date_time IS NOT NULL
+WHERE contato_telefone IS NOT NULL AND data_bloqueio IS NOT NULL
+-- WHERE phone IS NOT NULL AND date_time IS NOT NULL
