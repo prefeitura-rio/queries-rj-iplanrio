a748e4375f756831052fd081b386afca2d27ee1d
patriciacatandi <patriciabcatandi@gmail.com>
2025-08-25 14:37:38 -0300

    fix: blocklist rmi

--- models/intermediate/iplanrio/crm_registry/crm_whatsapp/int_crm_whatsapp_blocklist.sql
+++ models/intermediate/iplanrio/crm_registry/crm_whatsapp/int_crm_whatsapp_blocklist.sql
@@ -9,5 +9,12 @@
     }
 ) }}
 
-SELECT *
-FROM {{ ref('raw_iplanrio_wetalkie__blocklist') }}
+SELECT
+  CAST(phone AS STRING) as contato_telefone,
+  profile_name as contato_nome,
+  date_time as data_bloqueio,
+  reason as razao_bloqueio,
+  DATE(date_time) as data_particao
+-- TODO: mudar a origem do dado
+FROM `rj-crm-registry-dev.patricia___brutos_wetalkie.blocklist`
+WHERE phone IS NOT NULL AND date_time IS NOT NULL
