a27e63250b7e489a91b2efc8b709642565b76858
Luciano Muniz <luciano.muniz@prefeitura.rio>
2025-10-09 17:28:29 -0300

    Alterações no modelo de dados brutos do DAM (Dívida Ativa do Município).

---
+++ models/raw/pgm/divida_ativa/raw_divida_ativa_devedor_cda.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        schema="brutos_divida_ativa",
+        alias="devedor_dertidao_divida_ativa",
+        materialized="table",
+        tags=["raw", "devedor_cda", "cda", "devedor"],
+        description="Tabela que contém os registros que vinculam os contribuintes com as certidões de dívida ativa - CDAs. Para efeito de identificação do devedor principal, considera-se o tipo de vínculo 'P'. Pode haver mais de um devedor principal para uma mesma CDA."
+    )
+}}
+
+select safe_cast(numCDA as int64) as id_certidao_divida_ativa,
+  safe_cast(idpessoa as int64) as id_pessoa,
+  safe_cast(datsituacao as date) as data_criacao_registro,
+  safe_cast(tipvinculo as string) as codigo_tipo_vinculo,
+  case safe_cast(tipvinculo as string)
+    when 'P' then 'Principal'
+    when 'D' then 'Destinatário de correspondências'
+    else 'Outros'
+  end as nome_tipo_vinculo,
+    _prefect_extracted_at as loaded_at,
+    current_timestamp() as transformed_at
+from {{ source('brutos_divida_ativa_staging', 'DevedorCDA') }}
---
+++ models/raw/pgm/divida_ativa/raw_divida_ativa_pagamento_cda.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        schema="brutos_divida_ativa",
+        alias="pagamento_cda",
+        materialized="table",
+        tags=["raw", "divida_ativa", "tipo_pagamento", "pagamento", "guia"],
+        description="Tabela que dcontém o histórico de pagamento de cada cota de cada guia de pagamento vinculada a uma certidão de dívida ativa - CDA. Uma guia de pagamento pode conter mais de uma cota e cada cota pode estar vinculada a mais de uma CDA."
+    )
+}}
+
+select safe_cast(IDPagamentosCDA as int64) as id_pagamento_cda,
+  safe_cast(NumCDA as int64) as id_certidao_divida_ativa,
+  safe_cast(numGuiaPagamento as int64) as id_guia_pagamento,
+  safe_cast(numCotaPagamento as int64) as id_cota_guia_pagamento,
+  safe_cast(ObjPago as string) as codigo_objeto_pago,
+  case safe_cast(ObjPago as string)
+    when 'P' then 'Valor principal'
+    when 'H' then 'Honorários'
+    when 'G' then 'GRERJ'
+    else 'Não classificado'
+  end as nome_objeto_pago,
+  safe_cast(codTipoPagamento as int64) as codigo_tipo_pagamento,
+  ifnull(b.nome_tipo_pagamento, 'Não identificado') as nome_tipo_pagamento,
+  safe_cast(ValTotalPG as numeric) as valor_total_pago,
+  safe_cast(ValJurosGuiaPG as numeric) as valor_juros_guia_pago,
+  a._airbyte_extracted_at as loaded_at,
+  current_timestamp() as transformed_at
+from {{ source('brutos_divida_ativa_staging', 'PagamentosCDA') }} a
+left join {{ ref('raw_divida_ativa_tipo_pagamento_guia') }} b on b.id_tipo_pagamento = a.codTipoPagamento
