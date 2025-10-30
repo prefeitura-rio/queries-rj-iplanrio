5e54bb3c828916f7ef8e99931220a894868dfa87
Geraldo Maia <geraldo.maia@prefeitura.com>
2025-10-30 16:35:35 -0300

    atualizar id_localizacao_processo

--- models/raw/iplanrio/sicop/raw_sicop_localizacao_processo.sql
+++ models/raw/iplanrio/sicop/raw_sicop_localizacao_processo.sql
@@ -7,9 +7,9 @@
 
 -- Conversões e padronização de nomes conforme guia de estilo
 select
-  safe_cast(num_processo as string)    as id_processo,
-  safe_cast(localiza_processo as string) as localizacao_processo,
-  safe_cast(_prefect_extracted_at as date)                  as datalake_transformed_at  
+  safe_cast(num_processo as string)                          as id_processo,
+  safe_cast(localiza_processo as string)                     as id_localizacao_processo,
+  safe_cast(_prefect_extracted_at as date)                   as datalake_transformed_at  
 from {{ source('brutos_sicop_staging','localiza_processo') }}
 
 
