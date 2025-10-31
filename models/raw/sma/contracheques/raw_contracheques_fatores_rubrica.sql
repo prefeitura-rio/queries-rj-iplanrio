36d1e6d134e138e8f0a271b2e6d7d53e37843bac
Geraldo Maia <geraldo.maia@prefeitura.com>
2025-10-31 12:40:13 -0300

    AJUSTE FINO

--- models/raw/sma/contracheques/raw_contracheques_fatores_rubrica.sql
+++ models/raw/sma/contracheques/raw_contracheques_fatores_rubrica.sql
@@ -9,4 +9,4 @@ select
     safe_cast(FATOR as integer) as fator,
     safe_cast(RUBRICA as integer) as rubrica,
     safe_cast(_prefect_extracted_at as timestamp) as datalake_transformed_at
-FROM {{ source('brutos_contracheques_staging', 'FATORES_RUBRICA') }}
+FROM {{ source('brutos_contracheques_staging', 'FATORES_RUBRICAS') }}
