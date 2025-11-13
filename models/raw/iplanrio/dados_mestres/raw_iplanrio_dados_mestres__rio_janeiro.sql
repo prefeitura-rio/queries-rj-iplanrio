490e9439763afef038a08432ef00ab00d72de957
d116626 <d116626@gmail.com>
2025-11-13 15:29:37 -0300

    fear: add 1746 to datario

--- models/raw/iplanrio/dados_mestres/raw_iplanrio_dados_mestres__rio_janeiro.sql
+++ models/raw/iplanrio/dados_mestres/raw_iplanrio_dados_mestres__rio_janeiro.sql
@@ -6,7 +6,9 @@
 }}
 
 SELECT
-    SAFE_CAST(TRIM(mun) AS STRING) AS nome_municipio,
+    "3304557" AS id_municipio,
+    "RJ" AS sigla_uf,
+    "Rio de Janeiro" AS nome_municipio,
     SAFE_CAST(REGEXP_REPLACE(st_area_shape_, r',', '.') AS FLOAT64) AS area,
     SAFE_CAST(REGEXP_REPLACE(st_perimeter_shape_, r',', '.') AS FLOAT64) AS perimetro,
     SAFE_CAST(geometry AS STRING) AS geometry_wkt,
