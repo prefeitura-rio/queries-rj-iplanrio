05bbe0988fed145678499e659359e171f347255a
Luciano Muniz <luciano.muniz@prefeitura.rio>
2025-11-26 12:39:07 -0300

    Alterações em função da atividade de homologação dos dados.

--- models/mart/iplanrio/divida_ativa/mart_divida_ativa_contribuinte.sql
+++ models/mart/iplanrio/divida_ativa/mart_divida_ativa_contribuinte.sql
@@ -70,6 +70,28 @@ guias_contribuinte as
  (
   select a.id_pessoa, 
     count(distinct b.id_guia_pagamento) as quantidade_guias,
+    array_agg(
+      struct(
+        b.id_guia_pagamento, 
+        b.data_criacao_guia, 
+        b.numero_processo_associado,
+        b.tipo_guia,
+        b.tipo_pagamento,
+        b.tipo_receita,
+        b.situacao,
+        b.quantidade_cotas,
+        b.cotas
+      ) order by b.data_criacao_guia
+    ) as guias_pagamento
+  from contribuintes_vs_guias a
+  inner join {{ ref('mart_divida_ativa_guia_pagamento') }} b --`rj-iplanrio.divida_ativa.guia_pagamento`
+    on b.id_guia_pagamento = a.id_guia_pagamento
+  group by a.id_pessoa
+ ),
+
+ contagem_cotas as
+ (
+  select a.id_pessoa, 
     sum(
       case 
         when cotas.data_pagamento is null and cotas.data_vencimento >= current_date("America/Sao_Paulo")
@@ -111,24 +133,11 @@ guias_contribuinte as
         then 1
         else 0
       end 
-    ) as quantidade_cotas_pagas,
-    array_agg(
-      struct(
-        b.id_guia_pagamento, 
-        b.data_criacao_guia, 
-        b.numero_processo_associado,
-        b.tipo_guia,
-        b.tipo_pagamento,
-        b.tipo_receita,
-        b.situacao,
-        b.quantidade_cotas,
-        b.cotas
-      ) order by b.data_criacao_guia
-    ) as guias_pagamento
+    ) as quantidade_cotas_pagas
   from contribuintes_vs_guias a
   inner join {{ ref('mart_divida_ativa_guia_pagamento') }} b --`rj-iplanrio.divida_ativa.guia_pagamento`
     on b.id_guia_pagamento = a.id_guia_pagamento,
-    unnest(b.cotas) as cotas
+  unnest(b.cotas) as cotas
   group by a.id_pessoa
  )
 
@@ -140,15 +149,17 @@ select a.id_pessoa,
     b.saldo_devido_cdas,
     b.cdas_associadas,
     c.quantidade_guias,
-    c.valor_cotas_devidas_a_vencer,
-    c.quantidade_cotas_devidas_a_vencer,
-    c.valor_cotas_devidas_vencidas,
-    c.quantidade_cotas_devidas_vencidas,
-    c.valor_cotas_pagas,
-    c.quantidade_cotas_pagas,
+    d.valor_cotas_devidas_a_vencer,
+    d.quantidade_cotas_devidas_a_vencer,
+    d.valor_cotas_devidas_vencidas,
+    d.quantidade_cotas_devidas_vencidas,
+    d.valor_cotas_pagas,
+    d.quantidade_cotas_pagas,
     c.guias_pagamento
 from contribuintes a
 left join cdas_contribuinte b 
   on b.id_pessoa = a.id_pessoa
 left join guias_contribuinte c 
   on c.id_pessoa = a.id_pessoa
+left join contagem_cotas d
+  on d.id_pessoa = a.id_pessoa
