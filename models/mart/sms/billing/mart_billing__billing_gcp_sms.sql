aa2abb67bfbf6361e30253d8c8119ce70385cd9f
Pedro <pedrovitor.marques@gmail.com>
2025-11-27 08:17:02 -0300

    Add SMS billing model configuration to dbt_project.yml

---
+++ models/mart/sms/billing/mart_billing__billing_gcp_sms.sql
@@ -0,0 +1,26 @@
+{{
+  config(
+    alias="billing_gcp_sms",
+    materialized="table",
+    description="Consolidação de dados de billing do GCP dos ambientes geridos pela Saúde"
+  )
+}}
+with
+    source as (
+        select * 
+        from {{ ref("raw_billing_ncn__billing_gcp") }}
+    ),
+
+    billing_sms as (
+        select * 
+        from source 
+        where project_id in (
+            'rj-sms',
+            'rj-sms-dev',
+            'rj-sms-sandbox',
+            'rj-ivisa'
+        )
+    )
+
+select *
+from billing_sms
