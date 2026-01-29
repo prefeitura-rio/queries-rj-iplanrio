60b61d32f41e3e35dbfed081f7328c541b723565
Geraldo Maia <geraldo.maia@prefeitura.rio>
2026-01-29 10:49:17 -0300

    ajuste fino 2

--- models/raw/cgm/osinfo_rh/raw_osinfo_osinfo__categoria.sql
+++ models/raw/cgm/osinfo_rh/raw_osinfo_osinfo__categoria.sql
@@ -12,8 +12,8 @@ select
     safe_cast(`DS_CATEGORIA` as string) as descricao_categoria,
     safe_cast(`DT_CADASTRO` as datetime) as data_cadastro,
     safe_cast(`DT_ULTIMA_ATUALIZ` as datetime) as data_ultima_atualizacao,
-    safe_cast(`FLG_ATIVO` as char(1)) as flag_ativo,
-    safe_cast(`CNES_OBRIGATORIO` as char(1)) as flag_cnes_obrigatorio,
+    safe_cast(`FLG_ATIVO` as string) as flag_ativo,
+    safe_cast(`CNES_OBRIGATORIO` as string) as flag_cnes_obrigatorio,
     DATETIME(_prefect_extracted_at, "America/Sao_Paulo") AS datalake_loaded_at,
     DATETIME(current_timestamp(), "America/Sao_Paulo") AS datalake_transformed_at
 FROM {{ source('brutos_osinfo_rh_staging', 'categoria') }}
