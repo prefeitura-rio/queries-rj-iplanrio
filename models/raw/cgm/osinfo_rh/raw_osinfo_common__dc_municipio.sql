d5effc706f1a94ab7146401ec4a76ba2b8adf802
Geraldo Maia <geraldo.maia@prefeitura.rio>
2026-01-29 10:56:51 -0300

    ajuste 3

--- models/raw/cgm/osinfo_rh/raw_osinfo_common__dc_municipio.sql
+++ models/raw/cgm/osinfo_rh/raw_osinfo_common__dc_municipio.sql
@@ -10,6 +10,6 @@ select
     safe_cast(`MUNI_CD_IBGE` as decimal) as municipio_ibge_codigo,
     safe_cast(`UF_CD_IBGE` as decimal) as uf_ibge_codigo,
     safe_cast(`MUNI_DS_NOME` as string) as municipio_nome,
-    timestamp(_prefect_extracted_at, "America/Sao_Paulo") AS datalake_loaded_at,
-    timestamp(current_timestamp(), "America/Sao_Paulo") AS datalake_transformed_at
+    SAFE_CAST(SUBSTR(_prefect_extracted_at,1,10) AS DATE) AS datalake_transformed_at,
+    SAFE_CAST(SUBSTR(current_timestamp(),1,10) AS DATE) AS datalake_transformed_at
 FROM {{ source('brutos_osinfo_rh_staging', 'dc_municipio') }}
--- models/raw/cgm/osinfo_rh/raw_osinfo_common__dc_unidade_federacao.sql
+++ models/raw/cgm/osinfo_rh/raw_osinfo_common__dc_unidade_federacao.sql
@@ -10,6 +10,6 @@ select
     safe_cast(`UF_CD_IBGE` as decimal) as uf_ibge_codigo,
     safe_cast(`UF_SG_SIGLA` as string) as uf_sigla,
     safe_cast(`UF_DS_DESCRICAO` as string) as uf_descricao,
-    timestamp(_prefect_extracted_at, "America/Sao_Paulo") AS datalake_loaded_at,
-    timestamp(current_timestamp(), "America/Sao_Paulo") AS datalake_transformed_at
+    SAFE_CAST(SUBSTR(_prefect_extracted_at,1,10) AS DATE) AS datalake_transformed_at,
+    SAFE_CAST(SUBSTR(current_timestamp(),1,10) AS DATE) AS datalake_transformed_at
 FROM {{ source('brutos_osinfo_rh_staging', 'dc_unidade_federacao') }}
