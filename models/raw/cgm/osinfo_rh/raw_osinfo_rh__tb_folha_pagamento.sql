9e1d374bf0860744bc999817122e93e7b9d3c7b7
GeraldoSilvaMaiaJunior <geraldomaia20142@gmail.com>
2026-03-04 09:08:16 -0300

    Adiciona colunas de remuneração no modelo raw_osinfo_rh__tb_folha_pagamento
    - Inclui as colunas rem_bruta, descontos e rem_liquida no SQL e no YAML
    - Atualiza a consulta SQL para calcular a remuneração bruta, descontos e remuneração líquida a partir das tabelas relacionadas

--- models/raw/cgm/osinfo_rh/raw_osinfo_rh__tb_folha_pagamento.sql
+++ models/raw/cgm/osinfo_rh/raw_osinfo_rh__tb_folha_pagamento.sql
@@ -7,19 +7,31 @@
 }}
 
 select
-    safe_cast(`FPTO_CD_FOLHA` as int64) as folha_codigo,
-    safe_cast(`VINC_CD_VINCULO` as int64) as vinculo_codigo,
-    safe_cast(`FPTO_NR_MES_REFERENCIA` as int64) as mes_referencia,
-    safe_cast(`FPTO_NR_ANO_REFERENCIA` as int64) as ano_referencia,
-    safe_cast(`FPTO_NR_MES_COMPETENCIA` as int64) as mes_competencia,
-    safe_cast(`FPTO_NR_ANO_COMPETENCIA` as int64) as ano_competencia,
-    safe_cast(`ID_CONTRATO` as int64) as contrato_id,
-    safe_cast(`COD_UNIDADE` as int64) as unidade_codigo,
-    safe_cast(`FPTO_VL_CARGA_HORARIA_EFETIVA` as numeric) as carga_horaria_efetiva,
-    safe_cast(`FPTO_PERC_RATEIO` as numeric) as percentual_rateio,
-    safe_cast(`FPTO_DT_LICENCA_INICIO` as date) as licenca_inicio_data,
-    safe_cast(`FPTO_DT_LICENCA_FIM` as date) as licenca_fim_data,
-    safe_cast(`FPTO_OBSERVACAO` as string) as observacao,
-    safe_cast(SUBSTR(_prefect_extracted_at,1,10) AS datetime) AS datalake_loaded_at,
+    safe_cast(tfp.`FPTO_CD_FOLHA` as int64) as folha_codigo,
+    safe_cast(tfp.`VINC_CD_VINCULO` as int64) as vinculo_codigo,
+    safe_cast(tfp.`FPTO_NR_MES_REFERENCIA` as int64) as mes_referencia,
+    safe_cast(tfp.`FPTO_NR_ANO_REFERENCIA` as int64) as ano_referencia,
+    safe_cast(tfp.`FPTO_NR_MES_COMPETENCIA` as int64) as mes_competencia,
+    safe_cast(tfp.`FPTO_NR_ANO_COMPETENCIA` as int64) as ano_competencia,
+    safe_cast(tfp.`ID_CONTRATO` as int64) as contrato_id,
+    safe_cast(tfp.`COD_UNIDADE` as int64) as unidade_codigo,
+    safe_cast(tfp.`FPTO_VL_CARGA_HORARIA_EFETIVA` as numeric) as carga_horaria_efetiva,
+    safe_cast(tfp.`FPTO_PERC_RATEIO` as numeric) as percentual_rateio,
+    safe_cast(tfp.`FPTO_DT_LICENCA_INICIO` as date) as licenca_inicio_data,
+    safe_cast(tfp.`FPTO_DT_LICENCA_FIM` as date) as licenca_fim_data,
+    safe_cast(tfp.`FPTO_OBSERVACAO` as string) as observacao,
+    safe_cast(SUM(CASE WHEN tcv.rhgr_cd_codigo = 'P' THEN safe_cast(tvfp.vlfp_vl_valor AS NUMERIC) ELSE 0 END)
+  + SUM(CASE WHEN tcv.rhgr_cd_codigo = 'R' THEN safe_cast(tvfp.vlfp_vl_valor AS NUMERIC) ELSE 0 END) as numeric)
+    AS rem_bruta,
+
+    safe_cast(SUM(CASE WHEN tcv.rhgr_cd_codigo = 'D' THEN safe_cast(tvfp.vlfp_vl_valor AS NUMERIC) ELSE 0 END) as numeric)
+    AS descontos,
+    safe_cast((
+    SUM(CASE WHEN tcv.rhgr_cd_codigo IN ('P','R') THEN safe_cast(tvfp.vlfp_vl_valor AS NUMERIC) ELSE 0 END) - SUM(CASE WHEN tcv.rhgr_cd_codigo = 'D' THEN safe_cast(tvfp.vlfp_vl_valor AS NUMERIC) ELSE 0 END)
+    ) as numeric) AS rem_liquida,
+    safe_cast(SUBSTR(tfp.`_prefect_extracted_at`,1,10) AS datetime) AS datalake_loaded_at,
     safe_cast(current_timestamp()as datetime) AS datalake_transformed_at
-FROM {{ source('brutos_osinfo_rh_staging', 'tb_folha_pagamento') }}
+FROM {{ source('brutos_osinfo_rh_staging', 'tb_folha_pagamento') }} tfp
+left join {{ source('brutos_osinfo_rh_staging','tb_valores_folha_pagamento') }} tvfp on tfp.fpto_cd_folha = tvfp.fpto_cd_folha
+left join {{ source('brutos_osinfo_rh_staging','tb_colunas_valores') }} tcv on tvfp.rhco_cod_coluna = tcv.rhco_cod_coluna
+group by tfp.FPTO_CD_FOLHA, tfp.VINC_CD_VINCULO, tfp.FPTO_NR_MES_REFERENCIA, tfp.FPTO_NR_ANO_REFERENCIA, tfp.FPTO_NR_MES_COMPETENCIA, tfp.FPTO_NR_ANO_COMPETENCIA, tfp.ID_CONTRATO, tfp.COD_UNIDADE, tfp.FPTO_VL_CARGA_HORARIA_EFETIVA, tfp.FPTO_PERC_RATEIO, tfp.FPTO_DT_LICENCA_INICIO, tfp.FPTO_DT_LICENCA_FIM, tfp.FPTO_OBSERVACAO, tfp._prefect_extracted_at
