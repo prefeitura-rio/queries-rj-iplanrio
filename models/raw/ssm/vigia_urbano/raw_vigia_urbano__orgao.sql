d616d9421eb9345620d14d05f16718e75b3bbf6a
GeraldoSilvaMaiaJunior <geraldomaia20142@gmail.com>
2026-03-10 18:45:13 -0300

    Atualiza colunas nos modelos 'raw_vigia_urbano__orgao', 'raw_vigia_urbano__perfil_usuario' e 'raw_vigia_urbano__vw_resposta_ocorrencia' para maior clareza e consistência. As colunas foram renomeadas para refletir melhor suas funções e facilitar a compreensão dos dados.

--- models/raw/ssm/vigia_urbano/raw_vigia_urbano__orgao.sql
+++ models/raw/ssm/vigia_urbano/raw_vigia_urbano__orgao.sql
@@ -6,11 +6,11 @@
 }}
 
 SELECT
-    safe_cast(id_orgao AS int64) AS id_orgao,
-    safe_cast(TRIM(nome_orgao) AS string) AS nome_orgao,
-    safe_cast(codigo_orgao AS int64) AS codigo_orgao,
-    safe_cast(id_subprefeitura AS int64) AS id_subprefeitura,
-    safe_cast(TRIM(subprefeitura_json) AS string) AS subprefeitura_json,
+    safe_cast(orgaoID AS int64) AS id_orgao,
+    safe_cast(TRIM(orgaoID) AS string) AS nome_orgao,
+    safe_cast(codigo AS int64) AS codigo_orgao,
+    safe_cast(subPrefeitura AS int64) AS id_subprefeitura,
+    safe_cast(TRIM(subPrefJson) AS string) AS subprefeitura_json,
     safe.parse_datetime('%Y-%m-%d %H:%M:%E*S', _prefect_extracted_at) as datalake_loaded_at,
     safe_cast(current_timestamp() as datetime) AS datalake_transformed_at
 FROM {{ source('brutos_formulario_ocorrencia_staging', 'Orgao') }}
--- models/raw/ssm/vigia_urbano/raw_vigia_urbano__perfil_usuario.sql
+++ models/raw/ssm/vigia_urbano/raw_vigia_urbano__perfil_usuario.sql
@@ -6,11 +6,11 @@
 }}
 
 SELECT
-    safe_cast(id_perfil_usuario AS int64) AS id_perfil_usuario,
-    safe_cast(id_perfil_enum AS int64) AS id_perfil_enum,
-    safe_cast(id_usuario AS int64) AS id_usuario,
-    safe_cast(id_usuario_registro AS int64) AS id_usuario_registro,
-    safe_cast(data_registro AS datetime) AS data_registro,
+    safe_cast(perfilUsuarioID AS int64) AS id_perfil_usuario,
+    safe_cast(perfilEnum AS int64) AS id_perfil_enum,
+    safe_cast(usuarioID AS int64) AS id_usuario,
+    safe_cast(usuarioRegistroID AS int64) AS id_usuario_registro,
+    safe_cast(dataregistro AS datetime) AS data_registro,
     safe.parse_datetime('%Y-%m-%d %H:%M:%E*S', _prefect_extracted_at) as datalake_loaded_at,
     safe_cast(current_timestamp() as datetime) AS datalake_transformed_at
 FROM {{ source('brutos_formulario_ocorrencia_staging', 'PerfilUsuario') }}
