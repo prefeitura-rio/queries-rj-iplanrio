30024f5e6faf9d0ac0dca8a9894cfb40c9cdff41
lucianomunizfrancisco <luciano.muniz@prefeitura.rio>
2026-03-16 19:00:25 -0300

    Conclusão do modelo de dados do Ergon para a PGM, correções e migrações dos dados do Ergon da SMA e correções nos dados da CGM/OS_Info

--- models/raw/sma/brutos_recursos_humanos_ergon/raw_recursos_humanos_ergon__ficha_financeira_contabil.sql
+++ models/raw/sma/brutos_recursos_humanos_ergon/raw_recursos_humanos_ergon__ficha_financeira_contabil.sql
@@ -13,7 +13,7 @@
 }}
 
 SELECT
-    SAFE_CAST(DATE(mes_ano_folha) AS DATE) AS mes_ano_folha,
+    SAFE_CAST(mes_ano_folha AS DATE) AS mes_ano_folha,
     SAFE_CAST(REGEXP_REPLACE(TRIM(numfunc), r'\\.0$', '') AS int64) AS id_funcionario,
     SAFE_CAST(REGEXP_REPLACE(TRIM(numvinc), r'\\.0$', '') AS int64) AS id_vinculo,
     SAFE_CAST(REGEXP_REPLACE(TRIM(numpens), r'\\.0$', '') AS INT64) AS id_pensionista,
@@ -24,13 +24,13 @@ SELECT
     SAFE_CAST(TRIM(detalha) AS int64) AS detalhamento,
     SAFE_CAST(TRIM(rubrica) AS STRING) AS id_rubrica,
     SAFE_CAST(TRIM(tipo_rubrica) AS STRING) AS tipo_rubrica,
-    SAFE_CAST(TRIM(mes_ano_direito) AS date) AS mes_ano_direito,
+    SAFE_CAST(mes_ano_direito AS DATE) AS mes_ano_direito,
     SAFE_CAST(TRIM(desc_vant) AS int64) AS desconto_vantagem,
     SAFE_CAST(REGEXP_REPLACE(valor, r',', '.') AS FLOAT64) AS valor,
     SAFE_CAST(TRIM(complemento) AS STRING) AS observacao,
     SAFE_CAST(TRIM(tipo_classif) AS STRING) AS tipo_classificacao,
     SAFE_CAST(TRIM(classificacao) AS int64) AS classificacao,
     SAFE_CAST(REGEXP_REPLACE(TRIM(emp_codigo), r'\\.0$', '') AS STRING) AS id_empresa,
-    data_particao
+    SAFE_CAST(data_particao AS DATE) AS data_particao
 FROM {{ source('brutos_ergon_staging', 'IPL_PT_FICHAS') }} AS t
 
