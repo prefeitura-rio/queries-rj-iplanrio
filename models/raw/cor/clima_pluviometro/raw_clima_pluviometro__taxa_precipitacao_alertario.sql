37eaf67e7faf4229b51b25dc1810e2eb3e3346cf
Patrick Teixeira <105399231+tricktx@users.noreply.github.com>
2026-07-10 01:02:52 -0300

    fix(data): add data history in tables pluviometro (#283)
    * fix(data): add data history in tables pluviometro
    
    * fix(alias): fix name model
    
    * add cluster_by and fix query
    
    * fix(freshness): query

--- models/raw/cor/clima_pluviometro/raw_clima_pluviometro__taxa_precipitacao_alertario.sql
+++ models/raw/cor/clima_pluviometro/raw_clima_pluviometro__taxa_precipitacao_alertario.sql
@@ -2,41 +2,68 @@
     config(
         alias="taxa_precipitacao_alertario",
         materialized='incremental',
-        unique_key="primary_key",
+        unique_key='primary_key',
         partition_by={
             "field": "data_particao",
             "data_type": "date",
             "granularity": "day",
-        },    )
+        },
+        cluster_by=[
+            "primary_key",
+            "id_estacao"
+        ]
+    )
 }}
 
-SELECT
-    DISTINCT
-    CONCAT(id_estacao, '_', data_medicao) AS primary_key,
-    SAFE_CAST(id_estacao AS STRING) AS id_estacao,
-    SAFE_CAST(data_medicao AS DATETIME) AS data_medicao,
-    TIME(SAFE_CAST(data_medicao AS datetime)) AS horario,
-    SAFE_CAST(acumulado_chuva_5min AS FLOAT64) AS acumulado_chuva_5min,
-    SAFE_CAST(acumulado_chuva_10min AS FLOAT64) AS acumulado_chuva_10min,
-    SAFE_CAST(acumulado_chuva_15min AS FLOAT64) AS acumulado_chuva_15_min,
-    SAFE_CAST(acumulado_chuva_30min AS FLOAT64) AS acumulado_chuva_30min,
-    SAFE_CAST(acumulado_chuva_1h AS FLOAT64) AS acumulado_chuva_1h,
-    SAFE_CAST(acumulado_chuva_2h AS FLOAT64) AS acumulado_chuva_2h,
-    SAFE_CAST(acumulado_chuva_3h AS FLOAT64) AS acumulado_chuva_3h,
-    SAFE_CAST(acumulado_chuva_4h AS FLOAT64) AS acumulado_chuva_4h,
-    SAFE_CAST(acumulado_chuva_6h AS FLOAT64) AS acumulado_chuva_6h,
-    SAFE_CAST(acumulado_chuva_12h AS FLOAT64) AS acumulado_chuva_12h,
-    SAFE_CAST(acumulado_chuva_24h AS FLOAT64) AS acumulado_chuva_24h,
-    SAFE_CAST(acumulado_chuva_96h AS FLOAT64) AS acumulado_chuva_96h,
-    SAFE_CAST(acumulado_chuva_mes AS FLOAT64) AS acumulado_chuva_mes,
-    SAFE_CAST(ano_particao as INT64) ano_particao,
-    SAFE_CAST(mes_particao as INT64) mes_particao,
-    SAFE_CAST(data_particao as DATE) data_particao,
-FROM {{ source('clima_pluviometro_staging', 'taxa_precipitacao_alertario') }}
+with source_1 as (
+    SELECT DISTINCT
+        CONCAT(id_estacao, '_', data_medicao) AS primary_key,
+        SAFE_CAST(id_estacao AS STRING) AS id_estacao,
+        SAFE_CAST(data_medicao AS DATETIME) AS data_medicao,
+        TIME(SAFE_CAST(data_medicao AS DATETIME)) AS horario,
+        SAFE_CAST(acumulado_chuva_15min AS FLOAT64) AS acumulado_chuva_15_min,
+        SAFE_CAST(acumulado_chuva_1h AS FLOAT64) AS acumulado_chuva_1h,
+        SAFE_CAST(acumulado_chuva_4h AS FLOAT64) AS acumulado_chuva_4h,
+        SAFE_CAST(acumulado_chuva_24h AS FLOAT64) AS acumulado_chuva_24h,
+        SAFE_CAST(acumulado_chuva_96h AS FLOAT64) AS acumulado_chuva_96h,
+        SAFE_CAST(ano_particao AS INT64) AS ano_particao,
+        SAFE_CAST(mes_particao AS INT64) AS mes_particao,
+        SAFE_CAST(data_particao AS DATE) AS data_particao
+    FROM {{ source('clima_pluviometro_staging', 'taxa_precipitacao_alertario') }}
 
+    {% if is_incremental() %}
+        WHERE DATETIME(data_medicao) > (
+            SELECT MAX(DATETIME(data_medicao))
+            FROM {{ this }}
+        WHERE DATE(data_medicao) != '2099-03-03'
+    )
+    {% endif %}
+)
 
-{% if is_incremental() %}
+{% if not is_incremental() %}
 
-where DATETIME(data_medicao) > (SELECT MAX(DATETIME(data_medicao)) FROM {{ this }} where DATE(data_medicao) != '2099-03-03')
+, source_2 as (
+    SELECT DISTINCT
+        CONCAT(id_estacao, '_', data_medicao) AS primary_key,
+        SAFE_CAST(id_estacao AS STRING) AS id_estacao,
+        SAFE_CAST(data_medicao AS DATETIME) AS data_medicao,
+        TIME(SAFE_CAST(data_medicao AS DATETIME)) AS horario,
+        SAFE_CAST(acumulado_chuva_15_min AS FLOAT64) AS acumulado_chuva_15_min,
+        SAFE_CAST(acumulado_chuva_1_h AS FLOAT64) AS acumulado_chuva_1h,
+        SAFE_CAST(acumulado_chuva_4_h AS FLOAT64) AS acumulado_chuva_4h,
+        SAFE_CAST(acumulado_chuva_24_h AS FLOAT64) AS acumulado_chuva_24h,
+        SAFE_CAST(acumulado_chuva_96_h AS FLOAT64) AS acumulado_chuva_96h,
+        SAFE_CAST(ano AS INT64) AS ano_particao,
+        SAFE_CAST(mes AS INT64) AS mes_particao,
+        DATE_TRUNC(SAFE_CAST(data_medicao AS DATE), DAY) AS data_particao
+    FROM `rj-cor.clima_pluviometro_staging.taxa_precipitacao_alertario`
+)
 
+{% endif %}
+
+SELECT * FROM source_1
+
+{% if not is_incremental() %}
+UNION ALL
+SELECT * FROM source_2
 {% endif %}
