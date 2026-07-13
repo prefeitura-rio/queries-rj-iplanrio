19013c9943b19ac9e6b10b4f391bd3a00a834747
tricktx <Patrick.tx@hotmail.com.br>
2026-07-13 17:54:41 -0300

    fix(query):  adicionar tratamento para dados faltantes de precipitação entre 2025-01-01 e 2026-06-01

--- models/raw/cor/clima_pluviometro/raw_clima_pluviometro__taxa_precipitacao_alertario.sql
+++ models/raw/cor/clima_pluviometro/raw_clima_pluviometro__taxa_precipitacao_alertario.sql
@@ -41,7 +41,7 @@ with source_1 as (
 )
 
 {% if not is_incremental() %}
-
+-- Obtendo os dados de 1997 até 2024 de acordo com a tabela taxa_precipitacao_alertario no bucket de rj-cor
 , source_2 as (
     SELECT DISTINCT
         CONCAT(id_estacao, '_', data_medicao) AS primary_key,
@@ -57,6 +57,25 @@ with source_1 as (
         SAFE_CAST(mes AS INT64) AS mes_particao,
         DATE_TRUNC(SAFE_CAST(data_medicao AS DATE), DAY) AS data_particao
     FROM `rj-cor.clima_pluviometro_staging.taxa_precipitacao_alertario`
+),
+-- Como há um furo nos dados de 2025-01-01 até 2026-06-01 no bucket de rj-cor taxa_precipitacao_alertario, 
+-- Vamos completar os dados de acordo com a tabela taxa_precipitacao_alertario_5min no bucket de rj-cor que possui uma granularidade menor.
+source_3 as (
+    SELECT DISTINCT
+        CONCAT(id_estacao, '_', data_medicao) AS primary_key,
+        SAFE_CAST(id_estacao AS STRING) AS id_estacao,
+        SAFE_CAST(data_medicao AS DATETIME) AS data_medicao,
+        TIME(SAFE_CAST(data_medicao AS DATETIME)) AS horario,
+        SAFE_CAST(acumulado_chuva_15min AS FLOAT64) AS acumulado_chuva_15_min,
+        SAFE_CAST(acumulado_chuva_1h AS FLOAT64) AS acumulado_chuva_1h,
+        SAFE_CAST(acumulado_chuva_4h AS FLOAT64) AS acumulado_chuva_4h,
+        SAFE_CAST(acumulado_chuva_24h AS FLOAT64) AS acumulado_chuva_24h,
+        SAFE_CAST(acumulado_chuva_96h AS FLOAT64) AS acumulado_chuva_96h,
+        SAFE_CAST(ano_particao AS INT64) AS ano_particao,
+        SAFE_CAST(mes_particao AS INT64) AS mes_particao,
+        SAFE_CAST(data_medicao as DATE) as data_particao
+        FROM `rj-cor.clima_pluviometro_staging.taxa_precipitacao_alertario_5min`
+    where DATE(data_medicao) between '2025-01-01' and '2026-06-01'
 )
 
 {% endif %}
@@ -66,4 +85,6 @@ SELECT * FROM source_1
 {% if not is_incremental() %}
 UNION ALL
 SELECT * FROM source_2
+UNION ALL
+SELECT * FROM source_3
 {% endif %}
